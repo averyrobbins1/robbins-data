bde36a05ef87998750c356c80c19106e3d4ba692
averyrobbins1 <averyrobbins17@gmail.com>
2020-08-06 20:14:43 -0600

    h

---
+++ scripts/sweden.R
@@ -0,0 +1,29 @@
+# header start ------------------------------------------------------------
+#
+# script name: sweden.R
+#
+# script purpose:
+#
+# author: Avery Robbins
+#
+# date created: 2020-08-06
+#
+# notes:
+#
+#
+#
+#
+#
+# header end --------------------------------------------------------------
+
+
+# packages ----------------------------------------------------------------
+
+pacman::p_load(tidyverse, janitor)
+pacman::p_load_gh('averyrobbins1/sometools')
+
+# read data ---------------------------------------------------------------
+
+dat <- read_csv('')
+
+dat %>% glimpse()
