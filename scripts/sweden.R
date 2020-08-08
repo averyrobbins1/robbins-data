ccb338d3bd04a0f46daecc7e8cb3776b669496cd
averyrobbins1 <averyrobbins17@gmail.com>
2020-08-08 10:04:22 -0600

    m

--- scripts/sweden.R
+++ scripts/sweden.R
@@ -24,12 +24,12 @@ pacman::p_load_gh('averyrobbins1/sometools')
 
 # read data ---------------------------------------------------------------
 
-<<<<<<< HEAD
+
 dat <- read_xlsx('covid-data/swedish')
-=======
+
 dat <- read_xlsx('covid-data/swedish-covid-data-full.xlsx',
                  sheet = "age-group")
 
 write_csv(dat, 'covid-data/swedish-covid-data.csv')
->>>>>>> 168c20fc598e62e048c6ffb60a694c70f78fee94
+
 
